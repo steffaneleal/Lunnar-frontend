25b024e01054906fa2ef6b70baee5f22aa4b8648
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2026-02-19 23:48:53 -0300

    :sparkles: feat: criação dos componentes de footer e header para as telas de login e registro.

---
+++ components/AuthNavbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app-bar color="#1a1a22" theme="dark" flat border="b">
+    <v-container class="d-flex align-center py-0">
+      <NuxtLink to="/" class="d-flex align-center text-decoration-none text-h6 font-weight-bold text-primary mr-8">
+        <v-img
+          src="../public/lunnar-icon-light.png"
+          alt="Lunnar Logo"
+          width="32"
+          height="32"
+          class="mr-2"
+        ></v-img>
+        <span>Lunnar</span>
+      </NuxtLink>
+      <v-spacer></v-spacer>
+      <v-btn to="/login" :variant="isLoginPage ? 'tonal' : 'text'" :disabled="isLoginPage">
+        Login
+      </v-btn>
+      <v-btn to="/register" :variant="isRegisterPage ? 'tonal' : 'text'" :disabled="isRegisterPage">
+        Cadastre-se
+      </v-btn>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const isLoginPage = computed(() => route.path === '/login')
+const isRegisterPage = computed(() => route.path === '/register')
+</script>
---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-footer app color="#1a1a22" height="50" class="d-flex align-center justify-center">
+    <span class="text-white font-weight-light text-body-2">&copy; Lunnar 2026. Todos os direitos reservados</span>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
