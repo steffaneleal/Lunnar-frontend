18a0ca79214b9bac0acb599fb9b4ad187a12e48d
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2026-02-21 18:27:13 -0300

    :lipstick: feat: alteração de um detalhe no footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-footer app color="#1c64c4" height="50" class="d-flex align-center justify-center">
-    <span class="text-white font-weight-light text-body-2">&copy; Lunnar 2026. Todos os direitos reservados</span>
+    <span class="text-white font-weight-light text-body-2">&copy; Lunnar 2026. Todos os direitos reservados.</span>
   </v-footer>
 </template>
 
