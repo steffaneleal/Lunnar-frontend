76c666cc19ea70d730b3aca43ecc9bf3c942a116
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2026-02-22 20:00:26 -0300

    :lipstick: feat: Correção para o carrinho de pedidos aparecer apenas para os clientes, não para os administradores + nav do Dashboard.

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -15,10 +15,11 @@
       <v-btn variant="text" to="/">Início</v-btn>
       <v-btn variant="text" to="/products">Produtos</v-btn>
       <v-btn variant="text" to="/orders">Pedidos</v-btn>
+      <v-btn v-if="authStore.isAdmin" variant="text" to="/dashboard">Dashboard</v-btn>
       <v-btn v-if="authStore.isAdmin" variant="text" to="/customers">Clientes</v-btn>
       <v-btn v-if="authStore.isAdmin" variant="text" to="/admin">Administrador</v-btn>
       <v-spacer></v-spacer>
-      <v-btn to="/cart" icon>
+      <v-btn v-if="!authStore.isAdmin" to="/cart" icon>
         <v-badge :content="cartStore.itemCount" color="red">
           <v-icon>mdi-cart</v-icon>
         </v-badge>
